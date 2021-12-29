ff56af0df6ddcc816b6b199f31ad3ea8b35ff15a
suatogul <suatogul@gmail.com>
2021-12-29 21:45:18 +0100

    verbs were added in the index page

--- js/app.js
+++ js/app.js
@@ -4,3 +4,23 @@
  *     *Each row and column will be added by script
  * *
  */
+
+let irrVerbs = document.querySelector('#verblist');
+console.log(verblist);
+
+
+irrVerbs.innerHTML = verbList
+    .map((verb) => {
+        return `
+        <tr>
+        <th>${verb.verb1Name}</th>
+        <td>${verb.verb2Name}</td>
+        <td>${verb.verb3Name}</td>
+        <td>${verb.verb1Pronunciation}</td>
+        <td>${verb.verb2Pronunciation}</td>
+        <td>${verb.verb3Pronunciation}</td>
+        <td>${verb.verbTurkish}</td>
+      </tr>
+ `;
+    })
+    .join("");
