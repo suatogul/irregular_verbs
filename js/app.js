db5ea7511e232daed5dfe03282f97f1b18d69734
suatogul <suatogul@gmail.com>
2021-12-30 21:51:14 +0100

    exercise.html-from accardion list the function called for the turkish

--- js/app.js
+++ js/app.js
@@ -9,6 +9,9 @@
  *     *The same function will be called, when the page loaded. 
  *        *Otherwise inside the table will be emmpty when the page refreshed.
  * *Exercise page will be created
+ *     *Page will be designed
+ *     *Accordion verb list will be inserted when the page was loaded
+ *     *Each verb on the list will have an onclick() event to change the word on the exercise part  
  */
 
 const irrVerbs = document.querySelector('#verblist');
--- js/exercise.js
+++ js/exercise.js
@@ -1,7 +1,25 @@
 const accordionVerbList = document.querySelector('#accardion-verb-list');
-
-accordionVerbList.innerHTML = verbList.map((verb) => {
+const exerciseVerb = document.querySelector('#verb-holder');
+let max = verbList.length - 1;
+accordionVerbList.innerHTML = verbList.map((verb, index) => {
     return `
-    <div class="col-2 mb-3"><a href="#" onclick="">${verb.verb1Name}</a></div>
+    <div class="col-2 mb-3"><a href="#" role="button" onclick="newVerbExercise(${index})">${verb.verb1Name}</a></div>
     `;
 }).join("");
+
+
+function randomInt(min, max) {
+    return Math.floor((max - min + 1) * Math.random()) + min;
+}
+
+
+let index = randomInt(0, max);
+function newVerbExercise(indexNum) {
+    exerciseVerb.innerHTML =
+        `
+        ${verbList[indexNum].verbTurkish}
+ `;
+
+}
+
+newVerbExercise(index);
